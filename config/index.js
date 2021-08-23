deae0c811f7766570ed29ef6ef75172c83fc1304
Wendanly <2509531823@qq.com>
2021-08-23 19:51:30 +0800

    -

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
-// var baseUrl = 'http://172.32.149.193:30042/iot/' //开发
- var baseUrl = '/wuxidapinService/' //发布
+ var baseUrl = 'http://172.32.149.193:30042/iot/' //开发
+// var baseUrl = '/wuxidapinService/' //发布
